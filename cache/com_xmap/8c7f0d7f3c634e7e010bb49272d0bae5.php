2f06982498aff76950826ee210ad4a9c38a00b5b
Jarik <jarik11@mail.ru>
2012-02-08 00:11:56 +0400

    Added

---
+++ cache/com_xmap/8c7f0d7f3c634e7e010bb49272d0bae5.php
@@ -0,0 +1,68 @@
+<?php die(); ?>
+a:2:{s:6:"output";s:7751:"<div class="sitemap"><div class="componentheading"><h1>Карта сайта</h1></div><div class="contentpaneopen">
+<ul class="level_0">
+<li><a href="http://joost.net.ms/index.php?option=com_frontpage&amp;Itemid=1" title="Главная">Главная</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=5&amp;Itemid=6" title="Лицензия Joostina">Лицензия Joostina</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;Itemid=4" title="Ссылки">Ссылки</a>
+<ul class="level_1">
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;catid=2&amp;Itemid=4" title="Joostina!">Joostina!</a>
+<ul class="level_2">
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=2&amp;id=1&amp;Itemid=27" title="Joostina!">Joostina!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=2&amp;id=2&amp;Itemid=27" title="php.net">php.net</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=2&amp;id=3&amp;Itemid=27" title="MySQL">MySQL</a></li>
+</ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;catid=13&amp;Itemid=4" title="Joostina! по-русски">Joostina! по-русски</a>
+<ul class="level_2">
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=13&amp;id=6&amp;Itemid=27" title="Joom.Ru - Русский дом Joomla!">Joom.Ru - Русский дом Joomla!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=13&amp;id=7&amp;Itemid=27" title="Форумы Joomla!">Форумы Joomla!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=13&amp;id=8&amp;Itemid=27" title="Joomlaportal.ru!">Joomlaportal.ru!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=13&amp;id=9&amp;Itemid=27" title="Joomlaforum.ru">Joomlaforum.ru</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_weblinks&amp;task=view&amp;catid=13&amp;id=10&amp;Itemid=27" title="Joomla.ru">Joomla.ru</a></li>
+</ul>
+</li></ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_search&amp;Itemid=5" title="Поиск">Поиск</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_newsfeeds&amp;Itemid=7" title="Ленты новостей">Ленты новостей</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=category&amp;sectionid=3&amp;id=7&amp;Itemid=26" title="О Joostina (табличный вывод)">О Joostina (табличный вывод)</a>
+<ul class="level_1">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=13&amp;Itemid=26" title="Как помочь проекту">Как помочь проекту</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=1&amp;Itemid=26" title="Установка завершена!">Установка завершена!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=11&amp;Itemid=26" title="Планы по развитию">Планы по развитию</a></li>
+</ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_wrapper&amp;Itemid=8" title="В окне">В окне</a></li>
+<li class="active"><a href="http://joost.net.ms/index.php?option=com_xmap&amp;Itemid=27" title="Карта сайта">Карта сайта</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_poll&amp;Itemid=34" title="Опросы">Опросы</a></li>
+</ul>
+
+<ul class="level_0">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=blogsection&amp;id=1&amp;Itemid=29" title="Новости">Новости</a>
+<ul class="level_1">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=14&amp;Itemid=29" title="Joostina 1.3.0">Joostina 1.3.0</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=3&amp;Itemid=29" title="Краткая новость 2">Краткая новость 2</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=4&amp;Itemid=29" title="Краткая новость 3">Краткая новость 3</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=6&amp;Itemid=29" title="Пример новости 1">Пример новости 1</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=7&amp;Itemid=29" title="Пример новости 2">Пример новости 2</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=9&amp;Itemid=29" title="Пример новости 4">Пример новости 4</a>, <a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=9&amp;limit=1&amp;limitstart=1&amp;Itemid=29" >2 Страница</a>, <a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=9&amp;limit=1&amp;limitstart=2&amp;Itemid=29" >3 Страница</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=8&amp;Itemid=29" title="Пример новости 3">Пример новости 3</a></li>
+</ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=section&amp;id=2&amp;Itemid=30" title="Статьи">Статьи</a>
+<ul class="level_1">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=category&amp;sectionid=2&amp;id=15&amp;Itemid=30" title="В помощь веб-разработчику">В помощь веб-разработчику</a>
+<ul class="level_2">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=2&amp;Itemid=30" title="Система управления содержимым">Система управления содержимым</a></li>
+</ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=category&amp;sectionid=2&amp;id=3&amp;Itemid=30" title="Работа с Joostina CMS">Работа с Joostina CMS</a>
+<ul class="level_2">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=16&amp;Itemid=30" title="Административная панель Joostina - обзор основных элементов">Административная панель Joostina - обзор основных элементов</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=15&amp;Itemid=30" title="WYSIWYG редактор - что это?">WYSIWYG редактор - что это?</a></li>
+</ul>
+</li></ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=blogcategory&amp;id=7&amp;Itemid=32" title="О Joostina">О Joostina</a>
+<ul class="level_1">
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=13&amp;Itemid=32" title="Как помочь проекту">Как помочь проекту</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=1&amp;Itemid=32" title="Установка завершена!">Установка завершена!</a></li>
+<li><a href="http://joost.net.ms/index.php?option=com_content&amp;task=view&amp;id=11&amp;Itemid=32" title="Планы по развитию">Планы по развитию</a></li>
+</ul>
+</li><li><a href="http://joost.net.ms/index.php?option=com_contact&amp;Itemid=33" title="Контакты">Контакты</a></li>
+</ul>
+<div style="clear:left"></div></div></div>
+";s:6:"result";N;}
